c6440ab3487f1a373d632f79357258db4b176ea1
gaelbelami <ishimwegaelbelami@gmail.com>
2022-07-05 17:21:41 -0700

    Third method is a custom method based on Chi-Squared

--- own_impl_method.py
+++ own_impl_method.py
@@ -34,46 +34,48 @@ for imagePath in glob.glob(args["dataset"] + "\*.jpg"):
     hist = cv2.normalize(hist, hist).flatten()
     index[filename] = hist
 
-# METHOD #2: UTILIZING SCIPY
-# initialize the scipy methods to compute distances
-SCIPY_METHODS = (
-    ("Euclidean", dist.euclidean),
-    ("Manhattan", dist.cityblock),
-    ("Chebyshev", dist.chebyshev)
-)
 
-# loop over the comparison methods
-for (methodName, method) in SCIPY_METHODS:
-    # initialize the dictionary 
-    results = {}
+# METHOD #3: ROLL MY OWN METHOD
 
-    # loop over the index
-    for(k, hist) in index.items():
-        # compute the distance between the two histograms
-        # using the method and update the results dictionary
-        d = method(index["1.jpg"], hist)
-        results[k] = d
-    
-    # sort the results
-    results = sorted([(v, k) for (k, v) in results.items()])
+def chi2_distance(histA, histB, eps = 1e-10):
+    # Compute the chi-squared distance
+    d = 0.5 * np.sum([((a - b) ** 2) /  (a + b + eps) for (a, b) in zip(histA, histB)])
 
-    # show the query image
-    fig = plt.figure("Query")
-    ax = fig.add_subplot(1, 1, 1)
-    ax.imshow(images["1.jpg"])
+    # return the chi-squared distance
+    return d
+
+
+# initialize the resutls dictionary
+results = {}
+
+# loop over the index
+for (k, hist) in index.items():
+    # compute the distance between the two histograms
+    # uing the custom chi squared method, the update
+    # the results dictionary
+    d = chi2_distance(index["1.jpg"], hist)
+    results[k] = d
+
+# sort the resutls
+results = sorted([(v, k) for (k, v) in results.items()])
+
+# show the query image
+fig = plt.figure("Query")
+ax = fig.add_subplot(1, 1, 1)
+ax.imshow(images["1.jpg"])
+plt.axis("off")
+
+# initialize the results figure
+fig = plt.figure("Results: Custom Chi-Squared")
+fig.suptitle("Custom Chi-Squared", fontsize = 20)
+
+# loop over the results
+for (i, (v, k)) in enumerate(results):
+    # show the result
+    ax = fig.add_subplot(1, len(images), i + 1)
+    ax.set_title("%s: %.2f" % (k, v))
+    plt.imshow(images[k])
     plt.axis("off")
 
-    # initialize the results figure
-    fig = plt.figure("Results: %s" % (methodName) )
-    fig.suptitle(methodName, fontsize = 20)
-
-    # loop over the results
-    for (i, (v, k)) in enumerate(results):
-        # show the result
-        ax = fig.add_subplot(1, len(images), i + 1)
-        ax.set_title("%s: %.2f" % (k, v))
-        plt.imshow(images[k])
-        plt.axis("off")
-
-# Show the SciPy methods
+# Show the custom methods
 plt.show()
